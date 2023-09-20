8ff710b01cbf93dd2734c27a701df2beb7554865
Victor-python <64462520+Victor-python@users.noreply.github.com>
2023-09-20 23:24:16 +0300

    fix magic name

--- src/components/themes/OurThemes.vue
+++ src/components/themes/OurThemes.vue
@@ -36,7 +36,7 @@
                     <img src="../../images/magic-mobile.jpg" alt="blood-bowl" class='md:hidden w-full'>
                     <div className="gradient absolute top-0 bottom-0 left-0 right-0 bg-gradient-to-b from-transparent to-gray-900 grop-hover:from-gray-50 group-hover:to-white group-hover:opacity-70 duration-200"></div>
                     <h5 className="absolute px-6 duration-200 w52 bottom-4 md:bottom-8 md:px-10 group-hover:scale-110 group-hover:text-black">
-                        Blood Bowl
+                        Magic
                     </h5>
                 </div>
                 <div className="group relative overflow-hidden md:w-1/4">
