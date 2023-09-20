022e99500607955463a8634e4374b1eb0849a9f0
Victor-python <64462520+Victor-python@users.noreply.github.com>
2023-09-20 13:23:54 +0300

    tailwind installed and pages and components added

---
+++ src/UI/appLink/Applink.vue
@@ -0,0 +1,24 @@
+<template>
+  <div className="group">
+    <a href="/">
+      {{ content }}
+      <div className="mx-2 group-hover:border-b group hover:border-blue-50"></div>
+    </a>
+  </div>
+</template>
+
+<script>
+import { ref } from 'vue'
+export default {
+  setup(){
+    const content = ref('linkname');
+
+    return {
+      content,
+    }
+  }
+}
+</script>
+
+<style scoped>
+</style>
