1c3d08f82428a00a4365b981b5720f4f0763f985
Victor-python <64462520+Victor-python@users.noreply.github.com>
2023-09-20 14:20:51 +0300

    the footer component completed

--- src/layout/footer/TheFooter.vue
+++ src/layout/footer/TheFooter.vue
@@ -1,19 +1,109 @@
 <template>
   <h1>{{ content }}</h1>
+      <footer id="footer w-full">
+      <div className="py-10 mx-auto bg-black w-full">
+        <div className="flex flex-col items-center mb-8 space-y-6 md:flex-row md:space-y-0 md:justify-between md:items-start">
+          <div className="flex flex-col items-center space-y-8 md:items-start md:space-y-4">
+            <div className="logo h-8">
+              <Image name="logo" type="icon" classes="w-44 md:ml-3" />
+            </div>
+            <div className="flex flex-col items-center space-y-4 font-bold text-white md:flex-row md:space-y-0 md:space-x-6 md:ml-3">
+
+              <div className="h-10 group">
+                <a href="/">
+                  {{ footerlinks[0] }}
+                  <div className="mx-2 group-hover:border-b group hover:border-blue-50"></div>
+                </a>
+              </div>
+              <div className="h-10 group">
+                <a href="/">
+                {{ footerlinks[1] }}
+                  <div className="mx-2 group-hover:border-b group hover:border-blue-50"></div>
+                </a>
+              </div>
+              <div className="h-10 group">
+                <a href="/">
+                {{ footerlinks[2] }}
+                  <div className="mx-2 group-hover:border-b group hover:border-blue-50"></div>
+                </a>
+              </div>
+              <div className="h-10 group">
+                <a href="/">
+                {{ footerlinks[3] }}
+                  <div className="mx-2 group-hover:border-b group hover:border-blue-50"></div>
+                </a>
+              </div>
+              <div className="h-10 group">
+                <a href="/">
+                {{ footerlinks[4] }}
+                  <div className="mx-2 group-hover:border-b group hover:border-blue-50"></div>
+                </a>
+              </div>
+            </div>
+          </div>
+          <div className="flex flex-col items-start justify-between space-y-4-tex-gray-500">
+            <div className="flex items-center w-full justify-center space-x-4 md:justify-end md:mx-0">
+                <div className="h-8 group">
+                    <a href="/">
+                        <img src='../../images/icon-facebook.svg'  alt="selected img"  />
+                    </a>
+                </div>
+                <div className="h-8 group">
+                    <a href="/">
+                      
+                        <img src='../../images/icon-twitter.svg'  alt="selected img"  />
+                    </a>
+                </div>
+                <div className="h-8 group">
+                    <a href="/">
+                       
+                        <img src='../../images/icon-pinterest.svg'  alt="selected img"  />
+                    </a>
+                </div>
+                <div className="h-8 group">
+                    <a href="/">
+                       
+                        <img src='../../images/icon-instagram.svg'  alt="selected img"  />
+                    </a>
+                </div>
+            </div>
+
+            <div className="text-gray-500 mt-4">
+                &copy; {{ year }} MalditosDice. All rights reserved
+            </div>
+
+          </div>
+        </div>
+      </div>
+    </footer>
 </template>
 
 <script>
-import { ref } from 'vue'
+import { ref, reactive, computed } from 'vue';
+import { links } from '../../appsetup/appSetup'
 export default {
   setup(){
     const content = ref('Footer');
+    const year = computed(()=>{
+      {
+        return new Date().getFullYear();
+      }
+    });
+    const footerlinks = reactive(links);
+
+    console.log(footerlinks);
 
     return {
       content,
+      year,
+      footerlinks,
     }
   }
 }
 </script>
 
 <style scoped>
+a{
+  text-transform: capitalize;
+}
 </style>
