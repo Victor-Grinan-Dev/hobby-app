8a77b005ef30074dfc64a9ee1f80fa7e319fb1bc
Victor-python <64462520+Victor-python@users.noreply.github.com>
2023-09-20 21:02:07 +0300

    add to landing page

--- src/pages/landingPage/LandingPage.vue
+++ src/pages/landingPage/LandingPage.vue
@@ -2,19 +2,21 @@
   <hero-banner/>
   <about-us/>
   <the-collapse/>
-
+  <our-themes/>
 </template>
 
 <script>
 import { ref } from 'vue';
 import HeroBanner from '../../components/hero/HeroBanner';
 import AboutUs from '../../components/about/TheAbout';
-import TheCollapse from '../../components/collapse/TheCollapse'
+import TheCollapse from '../../components/collapse/TheCollapse';
+import OurThemes  from '../../components/themes/OurThemes';
 export default {
   components:{
     HeroBanner,
     AboutUs,
     TheCollapse,
+    OurThemes,
   },
   setup(){
     const content = ref('Landing Page');
