8d22751992f022486b9df29b39408433da038749
Victor-python <64462520+Victor-python@users.noreply.github.com>
2023-09-20 20:02:30 +0300

    about us section

--- src/pages/landingPage/LandingPage.vue
+++ src/pages/landingPage/LandingPage.vue
@@ -1,16 +1,20 @@
 <template>
   <hero-banner/>
   <about-us/>
+  <the-collapse/>
+
 </template>
 
 <script>
 import { ref } from 'vue';
 import HeroBanner from '../../components/hero/HeroBanner';
 import AboutUs from '../../components/about/TheAbout';
+import TheCollapse from '../../components/collapse/TheCollapse'
 export default {
   components:{
     HeroBanner,
     AboutUs,
+    TheCollapse,
   },
   setup(){
     const content = ref('Landing Page');
