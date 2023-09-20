5cb11a919fee0888148790fc084ce83d8a63e85d
Victor-python <64462520+Victor-python@users.noreply.github.com>
2023-09-20 19:48:57 +0300

    about us ready

--- src/pages/landingPage/LandingPage.vue
+++ src/pages/landingPage/LandingPage.vue
@@ -1,13 +1,16 @@
 <template>
   <hero-banner/>
+  <about-us/>
 </template>
 
 <script>
 import { ref } from 'vue';
 import HeroBanner from '../../components/hero/HeroBanner';
+import AboutUs from '../../components/about/TheAbout';
 export default {
   components:{
     HeroBanner,
+    AboutUs,
   },
   setup(){
     const content = ref('Landing Page');
