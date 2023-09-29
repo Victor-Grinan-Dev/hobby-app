c20c634d7aa04b1ea5976a2300dc73d25ef9ff96
Victor-python <64462520+Victor-python@users.noreply.github.com>
2023-09-29 11:47:22 +0300

    register btn component

--- src/main.js
+++ src/main.js
@@ -2,7 +2,11 @@ import { createApp } from "vue";
 import App from "./App.vue";
 import store from "./store/store";
 
+import MainBtn from "./components/appBtn/MainBtn.vue";
+
 const app = createApp(App);
+
+app.component("main-btn", MainBtn);
 
 app.use(store);
 // app.use(router);
