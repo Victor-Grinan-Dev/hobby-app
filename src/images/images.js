b037c7aefcaf91dab2d165ea19d6e401581daedb
Victor-python <64462520+Victor-python@users.noreply.github.com>
2023-09-29 11:12:03 +0300

    images func attempt

---
+++ src/images/images.js
@@ -0,0 +1,19 @@
+import strategoMobile from "./stratego-mobile.png";
+import strategoDesktop from "./stratego-desktop.png";
+import magicMobile from "./magic-mobile.jpg";
+import magicDesktop from "./magic-desktop.jpg";
+import campaigs40kMobile from "./40k-mobile.jpeg";
+import campaigs40kDesktop from "./stratego-desktop.png";
+import pooperBowlMobile from "./blood-bowl-mobile.jpeg";
+import pooperBowlDesktop from "./blood-bowl-desktop.jpeg";
+
+export const images = {
+  strategoMobile: strategoMobile,
+  strategoDesktop: strategoDesktop,
+  magicMobile: magicMobile,
+  magicDesktop: magicDesktop,
+  campaigs40kMobile: campaigs40kMobile,
+  campaigs40kDesktop: campaigs40kDesktop,
+  pooperBowlMobile: pooperBowlMobile,
+  pooperBowlDesktop: pooperBowlDesktop,
+};
--- src/pages/landingPage/themes/OurThemes.vue
+++ src/pages/landingPage/themes/OurThemes.vue
@@ -11,6 +11,20 @@
             </div>
 
             <div className="items-container">
+                <!--<div v-for='theme in themes' className="group relative overflow-hidden md:w-1/4" :key='theme.id'>
+                    <img :src="image(theme.desktopImg)" :alt='theme.id'
+                        class='w-full hidden md:block group-hover:scale-110 duration-200'>
+
+                    <img :src="image(theme.mobileImg)" :alt='theme.id' class='md:hidden w-full'>
+                    <div
+                        className="gradient absolute top-0 bottom-0 left-0 right-0 bg-gradient-to-b from-transparent to-gray-900 grop-hover:from-gray-50 group-hover:to-white group-hover:opacity-70 duration-200">
+                    </div>
+                    <h5
+                        className="absolute px-6 duration-200 w52 bottom-4 md:bottom-8 md:px-10 group-hover:scale-110 group-hover:text-black">
+                        Pooper Bowl league
+                    </h5>
+                </div>-->
+
                 <div className="group relative overflow-hidden md:w-1/4">
                     <img src="../../../images/blood-bowl-desktop.jpeg" alt="blood-bowl"
                         class='w-full hidden md:block group-hover:scale-110 duration-200'>
@@ -74,9 +88,11 @@
 </template>
 <script>
 import { ref } from 'vue';
+
 export default {
     setup() {
         const isOpen = ref(false);
+
 
         return {
             isOpen,
