8b25d77ee034116c57a14d9422e3ceb0afaebfa2
Victor-python <64462520+Victor-python@users.noreply.github.com>
2023-09-29 12:21:35 +0300

    themeItems

---
+++ src/pages/landingPage/themes/ThemeItem.vue
@@ -0,0 +1,23 @@
+<template>
+    <div className="group relative overflow-hidden md:w-1/4">
+        <img :src="theme.desktopImg" :alt="theme.id" class='w-full hidden md:block group-hover:scale-110 duration-200'>
+
+        <img :src="theme.mobileImg" :alt="theme.id" class='md:hidden w-full'>
+        <div
+            className="gradient absolute top-0 bottom-0 left-0 right-0 bg-gradient-to-b from-transparent to-gray-900 grop-hover:from-gray-50 group-hover:to-white group-hover:opacity-70 duration-200">
+        </div>
+        <h5
+            className="absolute px-6 duration-200 w52 bottom-4 md:bottom-8 md:px-10 group-hover:scale-110 group-hover:text-black">
+            {{ theme.name }}
+        </h5>
+    </div>
+</template>
+<script>
+export default {
+    props: ['theme'],
+    setup() {
+
+    }
+}
+</script>
+
