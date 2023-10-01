8f15bc180012c008227f9f16d41c12b1584ba1a7
Victor-python <64462520+Victor-python@users.noreply.github.com>
2023-10-01 13:42:31 +0300

    collapse index fix

--- src/pages/landingPage/about/TheAbout.vue
+++ src/pages/landingPage/about/TheAbout.vue
@@ -2,13 +2,13 @@
   <section id="features">
     <div className="relative container flex flex-col max-w-6xl mx-auto my-32 px-6 trext-gray-900 md:flex-row md:px-0 z-0">
 
-      <img src="../../../images/pngs/man&girl-boardgame.png" alt="nerds" class='z-1'>
+      <img src="../../../images/pngs/man&girl-boardgame.png" alt="nerds" class='z-0'>
       <div className="text-container top-48 pr-0 bg-white md:absolute md:right-0 md:py-20 md:pl-20 z-2">
         <h2
           className="max-w-lg mt-10 mb-6 font-sans text-3xl text-center text-gray-900 uppercase md:text-5xl md:text-left">
           {{ content }}
         </h2>
-        <p className="max-w-md text-center md:text-left">
+        <p className="max-w-md text-center md:text-left z-1">
           Founded in 2022, MalditosDice have been creating world-class
           events for some of the best boardgamers in Helsinki.
           Our yearly on-going events have expand the hobby of cards and boardgames
