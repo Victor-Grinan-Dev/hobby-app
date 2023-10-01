788e042669709202cb5263d6381ebaa0e643c65e
Victor-python <64462520+Victor-python@users.noreply.github.com>
2023-10-01 13:10:39 +0300

    clean comments

--- src/pages/landingPage/themes/OurThemes.vue
+++ src/pages/landingPage/themes/OurThemes.vue
@@ -14,68 +14,6 @@
                 <theme-item v-for='event in events' :name='event.name' :id='event.id' v-bind:key='event.id'
                     :desktopImg='event.desktopImg' :mobileImg='event.mobileImg'></theme-item>
 
-                <!--
-                <router-link to='/events/:pooperbowl' class='group relative overflow-hidden md:w-1/4'>
-
-                    <img src="../../../images/blood-bowl-desktop.jpeg" alt="blood-bowl"
-                        class='w-full hidden md:block group-hover:scale-110 duration-200'>
-
-                    <img src="../../../images/blood-bowl-mobile.jpeg" alt="blood-bowl" class='md:hidden w-full'>
-                    <div
-                        className="gradient absolute top-0 bottom-0 left-0 right-0 bg-gradient-to-b from-transparent to-gray-900 grop-hover:from-gray-50 group-hover:to-white group-hover:opacity-70 duration-200">
-                    </div>
-                    <h5
-                        className="absolute px-6 duration-200 w52 bottom-4 md:bottom-8 md:px-10 group-hover:scale-110 group-hover:text-black">
-                        Pooper Bowl league
-                    </h5>
-
-                </router-link>
-
-                <router-link to='/events/:40kcamp' className="group relative overflow-hidden md:w-1/4">
-                    <img src="../../../images/40k-desktop.jpeg" alt="blood-bowl"
-                        class='w-full hidden md:block group-hover:scale-110 duration-200'>
-
-                    <img src="../../../images/40k-mobile.jpeg" alt="blood-bowl" class='md:hidden w-full'>
-                    <div
-                        className="gradient absolute top-0 bottom-0 left-0 right-0 bg-gradient-to-b from-transparent to-gray-900 grop-hover:from-gray-50 group-hover:to-white group-hover:opacity-70 duration-200">
-                    </div>
-                    <h5
-                        className="absolute px-6 duration-200 w52 bottom-4 md:bottom-8 md:px-10 group-hover:scale-110 group-hover:text-black">
-                        40K campaigns
-                    </h5>
-
-                </router-link>
-
-                <router-link to='/events/:magic' className="group relative overflow-hidden md:w-1/4">
-                    <img src="../../../images/magic-desktop.jpg" alt="blood-bowl"
-                        class='w-full hidden md:block group-hover:scale-110 duration-200'>
-
-                    <img src="../../../images/magic-mobile.jpg" alt="blood-bowl" class='md:hidden w-full'>
-                    <div
-                        className="gradient absolute top-0 bottom-0 left-0 right-0 bg-gradient-to-b from-transparent to-gray-900 grop-hover:from-gray-50 group-hover:to-white group-hover:opacity-70 duration-200">
-                    </div>
-                    <h5
-                        className="absolute px-6 duration-200 w52 bottom-4 md:bottom-8 md:px-10 group-hover:scale-110 group-hover:text-black">
-                        Magic Brawls
-                    </h5>
-
-                </router-link>
-
-                <router-link to='/events/:others' className="group relative overflow-hidden md:w-1/4">
-                    <img src="../../../images/stratego-desktop.png" alt="blood-bowl"
-                        class='w-full hidden md:block group-hover:scale-110 duration-200'>
-
-                    <img src="../../../images/stratego-mobile.png" alt="blood-bowl" class='md:hidden w-full'>
-                    <div
-                        className="gradient absolute top-0 bottom-0 left-0 right-0 bg-gradient-to-b from-transparent to-gray-900 grop-hover:from-gray-50 group-hover:to-white group-hover:opacity-70 duration-200">
-                    </div>
-                    <h5
-                        className="absolute px-6 duration-200 w52 bottom-4 md:bottom-8 md:px-10 group-hover:scale-110 group-hover:text-black">
-                        Others
-                    </h5>
-
-                </router-link>
--->
             </div>
         </div>
 
