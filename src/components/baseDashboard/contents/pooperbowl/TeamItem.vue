578c164929c1a50627fddcac1786f5bccb941be3
Victor-python <64462520+Victor-python@users.noreply.github.com>
2023-10-03 14:45:35 +0300

    map team images

--- src/components/baseDashboard/contents/pooperbowl/TeamItem.vue
+++ src/components/baseDashboard/contents/pooperbowl/TeamItem.vue
@@ -2,27 +2,21 @@
     <li class='team-card bg-slate-500'>
         <router-link :to='"/events/pooperbowl/team/" + id'>
             {{ title }}
+            <img :src="teamLogo" alt="">
         </router-link>
     </li>
 </template>
 
 <script>
-import { ref, computed } from 'vue'
+import { ref } from 'vue'
 export default {
     props: ["id", 'title', 'logo'],
     setup(props) {
         const content = ref('Nothing here yet');
-        const logoURL = computed(() => {
-            return '../../../../assets/teamLogos/' + props.logo + '.png';
-        })
-        // const teamLogo = require(logoURL);
-        console.log(props.logo)
-        console.log(logoURL)
-        // console.log(teamLogo)
-
+        const teamLogo = ref(require('../../../../assets/teamLogos/' + props.logo + '.png'));
         return {
             content,
-
+            teamLogo,
         }
     }
 }
