cf8695f4bb0508aa0f7b799d672217bbdd1d525e
Victor-python <64462520+Victor-python@users.noreply.github.com>
2023-10-03 16:32:45 +0300

    fetch happened

--- src/store/modules/pooperbowl/actions.js
+++ src/store/modules/pooperbowl/actions.js
@@ -1,50 +1,51 @@
-const baseUrl = "https://docs.google.com/spreadsheets/d/";
+// const baseUrl = "https://docs.google.com/spreadsheets/d/";
 const sheetId = "1Z2CthPCjLsbVtx2g_48U73XzGq9B0qRvK9xl-R-B9gg";
-const constant = "/gviz/tq?sheet=";
-
+// const constant = "/gviz/tq?sheet=";
+import axios from "axios";
 export default {
-  async loadInstructions() {
+  async loadInstructions(context) {
     const sheetTitle = "Instructions";
     const sheetRange = "A1:M10";
 
-    await fetch(baseUrl + sheetId + constant + sheetTitle + sheetRange).then(
-      (res) => {
-        // parsedData = JSON.parse(res.data.substring(47).slice(0, -2)).table.rows;
-        console.log(res.data);
-      }
-    );
-    // context.commit("pooperBowl/setInstructions", data);
+    axios
+      .get(
+        "https://docs.google.com/spreadsheets/d/" +
+          sheetId +
+          "/gviz/tq?sheet=" +
+          sheetTitle +
+          sheetRange
+      )
+      .then((res) => {
+        const parsedData = JSON.parse(res.data.substring(47).slice(0, -2)).table
+          .rows;
+
+        const test1 = {
+          pageTitle: parsedData[0].c[0].v,
+          table: [
+            {
+              title: parsedData[1].c[0].v,
+              content: [
+                parsedData[1].c[1].v,
+                parsedData[2].c[1].v,
+                parsedData[3].c[1].v,
+              ],
+            },
+            {
+              title: parsedData[4].c[0].v,
+              content: [parsedData[4].c[1].v],
+            },
+            {
+              title: parsedData[5].c[0].v,
+              content: [parsedData[5].c[1].v],
+            },
+            {
+              title: parsedData[6].c[0].v,
+              content: [parsedData[6].c[1].v, parsedData[7].c[1].v],
+            },
+          ],
+        };
+        // console.log(test1);
+        context.commit("setInstruction", test1);
+      });
   },
-
-  //   async loadTeams(context) {
-  //     /**FETCH HERE */
-  //     const response = await fetch(url);
-  //     const resposeData = await response.json();
-  //     if (!response.ok) {
-  //       /** handle error */
-  //     }
-  //     const teams = [];
-  //     /** CONVERT DATA IN RIGHT FORMAT */
-  //     for (const key in resposeData) {
-  //       const team = {
-  //         id: resposeData[key].id,
-  //         team: resposeData[key].team,
-  //         race: resposeData[key].race,
-  //         coach: resposeData[key].coach,
-  //         gamesPlayed: resposeData[key].gamesPlayed,
-  //         w: resposeData[key].w,
-  //         l: resposeData[key].l,
-  //         d: resposeData[key].d,
-  //         tds: resposeData[key].tds,
-  //         cas: resposeData[key].cas,
-  //         points: resposeData[key].points,
-  //         color: resposeData[key].color,
-  //         logo: resposeData[key].logo,
-  //       };
-
-  //       teams.push(team);
-  //     }
-
-  //     context.commit("setTeams", teams);
-  //   },
 };
