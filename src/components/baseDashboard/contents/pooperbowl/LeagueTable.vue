5745d10f84c3695b77254448e1b38144a736b174
Victor-python <64462520+Victor-python@users.noreply.github.com>
2023-10-03 12:29:52 +0300

    conditional render if no teams

--- src/components/baseDashboard/contents/pooperbowl/LeagueTable.vue
+++ src/components/baseDashboard/contents/pooperbowl/LeagueTable.vue
@@ -2,27 +2,38 @@
     <div class='league-table'>
         <h1>{{ content }}</h1>
         <br>
-        <div v-for='team in filteredTeams' :key='team.id'>
-            <p>{{ team.team }}</p>
-            <br>
+        <ul v-if='hasTeams'>
+            <li v-for='team in filteredTeams' :key='team.id'>
+                <p>{{ team.team }}</p>
+            </li>
+        </ul>
+        <div v-else>
+            <p> No teams subscribed yet... </p>
         </div>
     </div>
 </template>
 
 <script>
-import { ref, reactive } from 'vue';
+import { ref, reactive, computed } from 'vue';
 import { useStore } from 'vuex';
 export default {
     setup() {
         const content = ref('League Table');
         const store = useStore();
-        const filteredTeams = reactive(store.getters['pooperBowl/leagueTable']);
+        const teams = reactive(store.getters['pooperBowl/leagueTable']);
+        const filteredTeams = computed(() => {
+            return store.getters['pooperBowl/leagueTable'];
+        })
+        const hasTeams = computed(() => {
+            return store.getters['pooperBowl/hasTeams'];
+        })
 
-        console.log(filteredTeams);
+        console.log(teams);
 
         return {
             content,
             filteredTeams,
+            hasTeams,
         }
     }
 }
--- src/store/modules/pooperbowl/getters.js
+++ src/store/modules/pooperbowl/getters.js
@@ -1,17 +1,28 @@
 export default {
+  /* this is all the meta data of the event */
   data(state) {
     return state.data;
   },
+
+  rules(state) {
+    return state.rules;
+  },
+
+  /** league table */
   leagueTable(state) {
     return state.leagueTable;
   },
+  hasTeams(state) {
+    return state.leagueTable && state.leagueTable.length > 0;
+  },
+
+  /** fixtures */
   fixtures(state) {
     return state.fixtures;
   },
+
+  /** matchups */
   matchups(state) {
     return state.matchups;
-  },
-  rules(state) {
-    return state.rules;
   },
 };
