b799225b85900cf10f0a15d8ed7a6a1ac6ae9f9b
Victor-python <64462520+Victor-python@users.noreply.github.com>
2023-10-04 15:01:12 +0300

    team card design

--- src/components/baseDashboard/contents/pooperbowl/TeamItem.vue
+++ src/components/baseDashboard/contents/pooperbowl/TeamItem.vue
@@ -4,13 +4,16 @@
             <div class='team-card'>
                 <div class='data-container'>
                     <div class='header-container'>
-                        <img src="#" alt="#" class='header-img'>
+                        <img src="../../../../images/pngs/gigant-dice.png" alt="header" class='header-img'>
                     </div>
-                    <div class='team-logo-container'>
-                        <img :src="logoURL" alt="#" class='team-logo'>
+                    <div class='player-color' :style='{ "background-color": color }'>
+                        <div class='team-logo-container'>
+                            <img :src="teamLogo" alt="#" class='team-logo'>
+                        </div>
                     </div>
-                    <p class='team-name'>Team name</p>
-                    <p class='team-race'>Team race</p>
+
+                    <p class='team-name' :style='{ "color": color }'>{{ title }}</p>
+                    <p class='team-race'>{{ race }}</p>
                     <div class='grafic-container'>
                         grafic
                     </div>
@@ -27,19 +30,21 @@
 import { ref } from 'vue';
 
 export default {
-    props: ["id", 'title', 'logo'],
+    props: ["id", 'title', 'logo', 'color', 'race'],
 
     setup(props) {
         const content = ref('Nothing here yet');
-        const logoURL = require(ref('../../../../assets/teamLogos/' + props.logo + '.png'));
-        // const teamLogo = require(logoURL);
+        const teamLogo = ref(require('../../../../assets/teamLogos/' + props.logo + '.png'));
+        // const teamLogo = require(teamLogo);
         console.log(props.logo)
-        console.log(logoURL)
+        console.log(teamLogo)
         // console.log(teamLogo)
+
 
         return {
             content,
-            logoURL,
+            teamLogo,
+
         }
     }
 }
@@ -47,9 +52,73 @@ export default {
 
 <style scoped>
 .team-card {
-    margin: 5px;
-    height: 50vh;
+    height: 65vh;
     width: 80vw;
-    max-width: 400px;
+    max-width: 300px;
+    position: relative;
+    /* padding: 5px; */
+}
+
+.data-container {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: space-between;
+    position: relative;
+    height: 65vh;
+}
+
+.header-container {
+    background-color: aquamarine;
+    position: relative;
+    top: 0;
+    height: 150px;
+    width: 100%;
+    clip-path: polygon(0% 0%, 100% 0%, 100% 75%, 0% 100%);
+}
+
+.player-color {
+    width: 120px;
+    height: 120px;
+    border-radius: 50%;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    position: absolute;
+    top: 80px;
+}
+
+.team-logo-container {
+    width: 100px;
+    height: 100px;
+    border-radius: 50%;
+    background-color: aliceblue;
+}
+
+.team-name {
+    font: 40px;
+    font-family: monospace;
+    position: relative;
+    top: 25px;
+    /* text-shadow: 0 0 5px rgb(253, 251, 251); */
+    background-color: rgba(0, 0, 0, 0.452);
+    padding: 0 5px;
+}
+
+.team-race {
+    font: 30px;
+    font-family: monospace;
+}
+
+.grafic-container {
+    background-color: blueviolet;
+    width: 70%;
+    height: 80px;
+}
+
+.card-footer {
+    background-color: black;
+    color: white;
+    width: 100%;
 }
 </style>
