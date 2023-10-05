01b04ef8b51bf1f0de01ea12aa648483c5737e3a
Victor-python <64462520+Victor-python@users.noreply.github.com>
2023-10-05 13:44:29 +0300

    404 minimal css

--- src/pages/notFound/NotFound.vue
+++ src/pages/notFound/NotFound.vue
@@ -1,12 +1,14 @@
 <template>
-  <h1>{{ content }}</h1>
+  <div class='four-o-four h-40 flex justify-center items-center'>
+    <h1 class='text'>{{ content }}</h1>
+  </div>
 </template>
 
 <script>
 import { ref } from 'vue'
 export default {
-  setup(){
-    const content = ref('404 Not found');
+  setup() {
+    const content = ref('404 Bro!');
 
     return {
       content,
@@ -16,4 +18,14 @@ export default {
 </script>
 
 <style scoped>
+.four-o-four {
+  background-image: url('../../images/pngs/many-dice.png');
+}
+
+.text {
+  font-family: monospace;
+  font-size: 40px;
+  font-weight: 900;
+  text-shadow: 0 0 10px white;
+}
 </style>
