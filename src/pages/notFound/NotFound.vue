84be987c40d1819db1d9f513b94620f33aeb1cca
Victor-python <64462520+Victor-python@users.noreply.github.com>
2023-10-08 10:53:53 +0300

    404 improved

--- src/pages/notFound/NotFound.vue
+++ src/pages/notFound/NotFound.vue
@@ -1,6 +1,6 @@
 <template>
-  <div class='four-o-four h-40 flex justify-center items-center'>
-    <h1 class='text'>{{ content }}</h1>
+  <div class='four-o-four flex flex-1 justify-center items-center'>
+    <h1 class='text px-6'>{{ content }}</h1>
   </div>
 </template>
 
@@ -8,7 +8,7 @@
 import { ref } from 'vue'
 export default {
   setup() {
-    const content = ref('404 Bro!');
+    const content = ref('404 Bro! You are rolling the wrong dice.');
 
     return {
       content,
@@ -20,12 +20,14 @@ export default {
 <style scoped>
 .four-o-four {
   background-image: url('../../images/pngs/many-dice.png');
+  object-fit: cover;
 }
 
 .text {
   font-family: monospace;
   font-size: 40px;
   font-weight: 900;
-  text-shadow: 0 0 10px white;
+  /* text-shadow: 0 0 10px white; */
+  background-color: rgba(240, 248, 255, 0.523);
 }
 </style>
