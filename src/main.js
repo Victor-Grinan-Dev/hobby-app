5cf02dd375bd1e9fdada4221cd79f48cdd8664e9
Victor-python <64462520+Victor-python@users.noreply.github.com>
2023-10-09 16:18:47 +0300

    register app-dialog global

--- src/main.js
+++ src/main.js
@@ -5,14 +5,15 @@ import store from "./store/store";
 import router from "./router/router";
 
 import MainBtn from "./components/appBtn/MainBtn.vue";
-
+import AppDialog from "./components/appDialog/appDialog.vue";
 const app = createApp(App);
 
 // const AppDialog = defineAsyncComponent(() =>
 //   import("./components/appDialog/AppDialog.vue")
 // );
+
 app.component("main-btn", MainBtn);
-// app.component("app-dialog", AppDialog);
+app.component("app-dialog", AppDialog);
 
 /**
 app.component("app-card", AppCard);
