cace5eb948626fa0005d7f0bb8c73b62bf5b8bcb
Victor-python <64462520+Victor-python@users.noreply.github.com>
2023-10-13 11:23:18 +0300

    close nav when changed component, make nav pos fix

--- src/components/sideNav/SideNav.vue
+++ src/components/sideNav/SideNav.vue
@@ -1,15 +1,15 @@
 <template>
-    <div class="side-navs-options">
+    <div class="side-navs-options text-white">
         <div class='static-side-nav relative hidden  min-h-full top-0 bottom-0 bg-pink-900 md:flex flex-col px-2 py-6'>
             <div class='side-item capitalize cursor-pointer' @click='switchComponents("fixtures-table")'>fixtures</div>
             <div class='side-item capitalize cursor-pointer' @click='switchComponents("news-feed")'>news feed</div>
             <div class='side-item capitalize cursor-pointer' @click='switchComponents("leader-boards")'>leaderboard</div>
             <div class='side-item capitalize cursor-pointer' @click='switchComponents("league-teams")'>teams</div>
         </div>
-        <div class='side-nav absolute top-0 bottom-0 bg-pink-900 flex flex-col px-2 py-6'
+        <div class='side-nav fixed top-0 bottom-0 bg-pink-900 flex flex-col px-2 py-6'
             :class='{ "-translate-x-full": isHidden }'>
 
-            <div class='handle absolute md:hidden bg-pink-900 hover:bg-pink-700 cursor-pointer' @click='toggleSideNav'>
+            <div class='handle absolute md:hidden bg-pink-900 hover:bg-pink-700 cursor-pointer pr-1' @click='toggleSideNav'>
                 {{ arrowDirection }}
             </div>
 
@@ -23,32 +23,27 @@
 </template>
 
 <script>
-import { computed, ref } from 'vue';
 export default {
     emits: ['switchComponent'],
-    setup() {
-        const content = ref('side nav');
-        const isHidden = ref(true);
-
-        const toggleSideNav = () => {
-            isHidden.value = !isHidden.value;
-        }
-
-        const arrowDirection = computed(() => {
-            return isHidden.value ? ">" : "<"
-        });
-
+    data() {
         return {
-            content,
-            isHidden,
-            arrowDirection,
-            toggleSideNav,
+            isHidden: true,
         }
     },
+
     methods: {
         switchComponents(comp) {
             this.$emit('switchComponent', comp);
+            this.toggleSideNav();
+        },
+        toggleSideNav() {
+            this.isHidden = !this.isHidden
         }
+    },
+    computed: {
+        arrowDirection() {
+            return this.isHidden ? ">" : "<"
+        },
     }
 }
 </script>
@@ -60,8 +55,12 @@ export default {
 
 .handle {
     height: 25px;
-    right: -11px;
+    right: -15px;
     top: 10px;
     border-radius: 0 100% 100% 0;
+}
+
+.side-navs-options {
+    z-index: 999;
 }
 </style>
