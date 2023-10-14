f08f20f544f85ee2195b6c7aefbb7bfc5f68a135
Victor-python <64462520+Victor-python@users.noreply.github.com>
2023-10-14 11:51:44 +0300

    reset news feed component

--- src/pages/pooperbowl/dashboard/components/newsFeed/NewsFeed.vue
+++ src/pages/pooperbowl/dashboard/components/newsFeed/NewsFeed.vue
@@ -1,38 +1,14 @@
 <template>
-    <div class='news-container max-h-screen'>
-        <div class="feed relative flex flex-col mb-3" v-for='feed in feeds' :key='feed.id'>
-
-            <div class=' flex flex-col items-center md:items-start bg-gray-500 mb-1 p-2 rounded-xl'>
-                <div class='flex items-center mb-1 w-full'>
-                    <div class='user-img relative w-10 h-10 rounded-full bg-black ml-1'></div>
-
-                    <p class="username relative capitalize ml-1">{{ feed.author }}:</p>
-                </div>
-                <div class='image-container h-40 w-60 md:h-64 md:w-64 bg-black'></div>
-                <div>
-                    <p>{{ feed.content }}</p>
-                </div>
-                <div class="likes-area flex" v-if='feed.likes'>
-                    <div class='likes'>❤️</div>
-                    <div class='likes-amount'>{{ feed.likes }}</div>
-                </div>
-            </div>
-
-            <div class='comment-item relative flex items-center rounded-full bg-gray-500 mb-1'
-                v-for='comment in feed.comments' :key='comment.id'>
-                <div class='user-img relative w-10 h-10 rounded-full bg-black ml-1'></div>
-                <div class='pl-2'>
-                    <p class="username relative capitalize">{{ comment.author }}:</p>
-                    <p class='comment-content relative '>{{ comment.comment }}</p>
-                </div>
-            </div>
-        </div>
-    </div>
+    <base-component>
+        <p class='bg-blue-500'>{{ content || "News Feed" }}</p>
+    </base-component>
 </template>
 
 <script>
 import { ref } from 'vue';
+import BaseComponent from '../BaseComponent.vue';
 export default {
+    components: { BaseComponent, },
     data() {
         return {
             feeds: [
@@ -87,6 +63,37 @@ export default {
         }
     }
 }
+/*
+    <div class='news-container max-h-screen'>
+        <div class="feed relative flex flex-col mb-3" v-for='feed in feeds' :key='feed.id'>
+
+            <div class=' flex flex-col items-center md:items-start bg-gray-500 mb-1 p-2 rounded-xl'>
+                <div class='flex items-center mb-1 w-full'>
+                    <div class='user-img relative w-10 h-10 rounded-full bg-black ml-1'></div>
+
+                    <p class="username relative capitalize ml-1">{{ feed.author }}:</p>
+                </div>
+                <div class='image-container h-40 w-60 md:h-64 md:w-64 bg-black'></div>
+                <div>
+                    <p>{{ feed.content }}</p>
+                </div>
+                <div class="likes-area flex" v-if='feed.likes'>
+                    <div class='likes'>❤️</div>
+                    <div class='likes-amount'>{{ feed.likes }}</div>
+                </div>
+            </div>
+
+            <div class='comment-item relative flex items-center rounded-full bg-gray-500 mb-1'
+                v-for='comment in feed.comments' :key='comment.id'>
+                <div class='user-img relative w-10 h-10 rounded-full bg-black ml-1'></div>
+                <div class='pl-2'>
+                    <p class="username relative capitalize">{{ comment.author }}:</p>
+                    <p class='comment-content relative '>{{ comment.comment }}</p>
+                </div>
+            </div>
+        </div>
+    </div>
+*/
 </script>
 
 <style scoped>
