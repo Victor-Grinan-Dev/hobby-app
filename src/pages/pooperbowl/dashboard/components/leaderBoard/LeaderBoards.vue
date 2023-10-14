9cb064785ff9ca2fc0bc7f64c395c915bf0d20e7
Victor-python <64462520+Victor-python@users.noreply.github.com>
2023-10-14 12:13:42 +0300

    news feed working

--- src/pages/pooperbowl/dashboard/components/leaderBoard/LeaderBoards.vue
+++ src/pages/pooperbowl/dashboard/components/leaderBoard/LeaderBoards.vue
@@ -1,6 +1,7 @@
 <template>
     <div class="leaderboard-component pr-2 md:pr-10">
-        <div class='bg-purple-700'>tabs/filters?</div>
+
+        <div class='bg-purple-700'>{{ content }} tabs/filters</div>
         <div
             class='leaderboards-container bg-slate-500 flex flex-col md:flex-row flex-wrap justify-center md:justify-start py-2 items-center md:items-start gap-1 md:px-2'>
 
